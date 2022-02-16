045570234ddcd9c8ab1c984340569ac0116fbb95
dsebastiansandoval <dsebastiansandoval@gmail.com>
2022-02-16 11:57:29 -0500

    ShipitTest / Final controller ships made

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,29 +1,31 @@
 class ShippingsController < ApplicationController
 
-  def index
-
-
+  def initialize()
+    super
+    @all_orders = Order.all
+    @shippments = Shipping.all
   end
+
 
   def new
       if Order.exists?(order_id: params[:locat])
-     
-     end
-     order = Order.find_by(order_id: params[:locat])
-     @shipping = Shipping.where(order_id: order.id).first_or_create()
-     @shipping.update(order_id: order.id)
+        order = Order.find_by(order_id: params[:locat])
+        @shipping = Shipping.where(order_id: order.id).first_or_create()
+        @shipping.update(order_id: order.id)
+        @shipping.update(status: "OK")
 
-     if @shipping.save
+        if @shipping.save
 
-       redirect_to '/'
-     else
-       redirect_to '/'
-     end
+          redirect_to '/show_ships'
+        else
+          redirect_to '/'
+        end
+
+      else
+        redirect_to '/'
+      end
     
   end
 
-  def create
-    
-  end
 
 end
